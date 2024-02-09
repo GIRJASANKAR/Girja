3c81563bdfb97c4ddc0bbce7b88c50184a909617
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2024-02-09 10:33:40 +0530

    custom-validator logic

---
+++ src/custom-validator/custom-validator-component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-custom-validator',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './custom-validator.component.html',
+  styleUrls: ['./custom-validator.component.css'],
+})
+export class CustomValidatorComponent {
+  public loginForm!: FormGroup;
+  constructor(public fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: new FormControl('initialValue', [Validators.required]),
+      password: new FormControl('initialPassword', [Validators.required]),
+    });
+  }
+
+
+  submitForm(){
+    console.log(this.loginForm.value);
+    this.loginForm.controls['username'].setValue("");
+    this.loginForm.controls['password'].setValue("");
+  }
+}
