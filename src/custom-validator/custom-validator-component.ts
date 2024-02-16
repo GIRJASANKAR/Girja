c3b1e06b00bad066710625f8697644ac62a04c83
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2024-02-16 12:15:35 +0530

    .

--- src/custom-validator/custom-validator-component.ts
+++ src/custom-validator/custom-validator-component.ts
@@ -28,6 +28,7 @@ export class CustomValidatorComponent {
 // needs to add the directive
   submitForm(){
     
+    
     this.loginForm.controls['username'].setValue("");
     this.loginForm.controls['password'].setValue("");
   }
