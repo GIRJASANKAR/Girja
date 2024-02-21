79737b6817e2365e65e24703d127a0e088520c19
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2024-02-21 12:15:55 +0530

    added holy grail component ts ui logic

---
+++ src/app/ui/holy-grail/holygrail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-holy-grail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './holy-grail.component.html',
+  styleUrls: ['./holy-grail.component.css']
+})
+export class HolyGrailComponent {
+
+}
